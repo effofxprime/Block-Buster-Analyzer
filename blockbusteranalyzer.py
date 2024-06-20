838ef986c4620effd4f583f6b83f810940f4d4f9
Erialos <effofxprime@gmail.com>
2024-06-20 13:16:39 -0600

    Update blockbusteranalyzer.py

--- blockbusteranalyzer.py
+++ blockbusteranalyzer.py
@@ -9,7 +9,7 @@
 # @Date - 2024-06-06 15:19:00 UTC
 # @Last_Modified_By - Jonathan - Erialos
 # @Last_Modified_Time - 2024-06-21 15:19:00 UTC
-# @Version - 1.0.17
+# @Version - 1.0.16
 # @Description - This script analyzes block sizes in a blockchain and generates various visualizations.
 
 # LOCKED - Only edit when we need to add or remove imports
@@ -137,7 +137,7 @@ def process_block(height, endpoint_type, endpoint_url):
             return None
 
         block_size = len(json.dumps(block_info))
-        block_size_mb = block_size / 1048576
+        block_size_mb = block_size / 1048576  # Convert size to MB
         block_time = parse_timestamp(block_info['result']['block']['header']['time'])
         return (height, block_size_mb, block_time)
     except Exception as e:
@@ -382,28 +382,23 @@ def main():
 
     futures = [
         executor.submit(process_block, height, connection_type, endpoint_url)
-        for height in range(lower_height, upper_height + 1, json_workers)
+        for height in range(lower_height, upper_height + 1)
     ]
-
-    error_count = 0
+    completed = 0
     for future in as_completed(futures):
         if shutdown_event.is_set():
             print(f"{bash_color_red}Shutdown event detected. Exiting...{bash_color_reset}")
             sys.exit(0)
         result = future.result()
         if result:
-            block_data.append({"height": result[0], "size": float(result[1]), "time": result[2]})
-        else:
-            error_count += 1
-        completed = len(block_data)
+            height, size, time = result
+            block_data.append({"height": height, "size": size, "time": time})
+        completed += 1
         progress = (completed / total_blocks) * 100
         elapsed_time = tm.time() - start_script_time
         estimated_total_time = elapsed_time / completed * total_blocks
         time_left = estimated_total_time - elapsed_time
         print(f"{bash_color_light_blue}Progress: {progress:.2f}% ({completed}/{total_blocks}) - Estimated time left: {timedelta(seconds=int(time_left))}", end='\r')
-        if error_count > total_blocks * 0.1:  # Exiting if more than 10% errors
-            print(f"{bash_color_red}Too many errors fetching data. Exiting...{bash_color_reset}")
-            sys.exit(1)
 
     executor.shutdown(wait=True)
 
@@ -426,7 +421,7 @@ def main():
     }
 
     for height, size, time in block_data:
-        block = {"height": height, "size": float(size), "time": time}
+        block = {"height": height, "size": size, "time": time}
         categorize_block(block, categories)
 
     data = {
